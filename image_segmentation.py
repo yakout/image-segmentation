c3cb2511359809ad1763c4b5f5d121554cc76c99
Ahmed Yakout <iyakout@hotmail.com>
2018-12-27 02:14:23 +0200

    Enhance metrics and add visualize and verbose options

--- image_segmentation.py
+++ image_segmentation.py
@@ -2,9 +2,9 @@ from sklearn.cluster import SpectralClustering
 from skimage.transform import resize
 from scipy.misc import imresize
 
-
 from k_means import KMeans
 from utils import *
+from metrics import *
 
 def kmeans_segment(img, n_clusters=DEFAULT_N_CLUSTERS, max_iter=K_MEANS_DEFAULT_MAX_ITER):
     original_img_shape = img.shape
@@ -71,7 +71,8 @@ def spectral_segment_images(X, y=None, n_clusters=n_clusters,
                                gamma=1,
                                include_spatial=False,
                                cache=False, load_from_cache=False,
-                               visualize=False):
+                               visualize=False,
+                               verbose=False):
     """
     n_clusters : array of K values
     will return (N, K, d)
@@ -81,17 +82,22 @@ def spectral_segment_images(X, y=None, n_clusters=n_clusters,
     visualize : visualize the original images with the segmented images against
                 its ground truth images. (Must provide y)
     """
+    clustered_images = []
+
     if load_from_cache:
-        return np.load('./cache/spectral_segment_images.npy')
+        clustered_images = np.load('./cache/spectral_segment_images.npy').tolist()
     Ks = n_clusters
     N = X.shape[0]
-    clustered_images = []
     for i in range(N):
         img = X[i]
         img_size = (img.shape[0], img.shape[1])
         K_images = []
-        for k in Ks:
-            res = spectral_segment(img, n_clusters=k,
+        for j, k in enumerate(Ks):
+            res = None
+            if load_from_cache:
+                res = clustered_images[i][j]
+            else:
+                res = spectral_segment(img, n_clusters=k,
                                         affinity=affinity,
                                         n_neighbors=n_neighbors,
                                         gamma=gamma,
@@ -109,23 +115,30 @@ def spectral_segment_images(X, y=None, n_clusters=n_clusters,
         
 
 def kmeans_segment_images(X, y=None, n_clusters=n_clusters, cache=False,
-                                     load_from_cache=False, visualize=False):
+                                     load_from_cache=False, visualize=False,
+                                     verbose=False):
     """
     will return (N, K, d)
     """
+    images = []
     if load_from_cache:
-        return np.load('./cache/kmeans_segment_images.npy')
+        images = np.load('./cache/kmeans_segment_images.npy').tolist()
     Ks = n_clusters
     N = X.shape[0]
 #     print("N = {}".format(N))
-    images = []
     for i in range(N):
         img = X[i]
         img_size = (img.shape[0], img.shape[1])
         K_images = []
         for j, k in enumerate(Ks):
-            clusters, _ = kmeans_segment(img, n_clusters=k)
-            seg_image = clusters.reshape(img_size)
+            seg_image = None
+            if load_from_cache:
+                seg_image = images[i][j]
+            else:
+                clusters, _ = kmeans_segment(img, n_clusters=k)
+                seg_image = clusters.reshape(img_size)
+            if verbose:
+                print("kmeans_segment_images: segmented image i = {} with k = {} ".format(i, k))
             if visualize and y is not None:
                 visualize_result(X[i], seg_image, y[i])
             K_images.append(seg_image)
@@ -133,10 +146,114 @@ def kmeans_segment_images(X, y=None, n_clusters=n_clusters, cache=False,
     images = np.array(images)
     if cache:
         np.save('./cache/kmeans_segment_images.npy', images)
-    # print(images.shape)
+    if verbose:
+        print("kmeans_segment_images: images.shape = {} ".format(images.shape))
     return images
 
 
-def big_picture(X, y, n_images=5, n_clusters=[5]):
-    kmeans_segment_images(X[0:n_images], y=y, n_clusters=n_clusters, cache=True, visualize=True)
-    spectral_segment_images(X[0:n_images], y=y, n_clusters=n_clusters, cache=True, visualize=True) # 5-NN graph
+def big_picture(X, y, n_images=5, n_clusters=[5], load_from_cache=False):
+    kmeans_segment_images(X[0:n_images], y=y,
+                                         n_clusters=n_clusters,
+                                         cache=True,
+                                         load_from_cache=load_from_cache,
+                                         visualize=True)
+    spectral_segment_images(X[0:n_images], y=y,
+                                           n_clusters=n_clusters,
+                                           cache=True,
+                                           load_from_cache=load_from_cache,
+                                           visualize=True) # 5-NN graph
+
+def eval_kmeans_results(X, y, n_clusters=n_clusters,
+                     n_images=None,
+                     cache=False,
+                     load_from_cache=False,
+                     visualize=False,
+                     eval_fn='f1',
+                     verbose=False):
+    """
+    X : images raw data
+    y : ground truth images
+    n_images : number of images to evaluate k-means on, if None it will evaluate
+               on all images (default).
+    eval_fn : the evaluation function: 'cond_enropy' or 'f1' (default is 'f1')
+    """
+    segmented_images = kmeans_segment_images(X[0:n_images],
+                                            y=y,
+                                            n_clusters=n_clusters,
+                                            cache=cache,
+                                            load_from_cache=load_from_cache,
+                                            visualize=visualize,
+                                            verbose=verbose
+                                            )
+    avg_for_each_K = [] # average eval for all images for each K
+
+    K = segmented_images.shape[1]
+    if eval_fn == 'cond_entropy':
+        for i in range(K):
+            results = conditional_entropy_all(segmented_images[:, i], y)
+            avg_value = np.average(results)
+            max_value = np.max(results)
+            min_value = np.min(results)
+            avg_for_each_K.append(results)
+            if verbose:
+                print("average value of {} at K = {} is: avg: {}, max: {}, min: {}".format(eval_fn, n_clusters[i], avg_value, max_value, min_value))
+    elif eval_fn == 'f1':
+        for i in range(K):
+            results = f1_score_all(segmented_images[:, i], y)
+            avg_value = np.average(results)
+            max_value = np.max(results)
+            min_value = np.min(results)
+            avg_for_each_K.append(results)
+            if verbose:
+                print("average value of {} at K = {} is: avg: {}, max: {}, min: {}".format(eval_fn, n_clusters[i], avg_value, max_value, min_value))
+    else:
+        raise ValueError('invalid \'eval_fn\' value: expected: (f1 or cond_entropy)')
+
+    return avg_for_each_K
+
+def eval_spectral_results(X, y, n_clusters=n_clusters,
+                                n_images=None,
+                                cache=False,
+                                load_from_cache=False,
+                                visualize=False,
+                                eval_fn='f1',
+                                verbose=False):
+    """
+    X : images raw data
+    y : ground truth images
+    n_images : number of images to evaluate k-means on, if None it will evaluate
+               on all images (default).
+    eval_fn : the evaluation function: 'cond_enropy' or 'f1' (default is 'f1')
+    """
+    segmented_images = spectral_segment_images(X[0:n_images],
+                                                y=y,
+                                                n_clusters=n_clusters,
+                                                cache=cache,
+                                                load_from_cache=load_from_cache,
+                                                visualize=visualize,
+                                                verbose=verbose)
+    avg_for_each_K = [] # average eval for all images for each K
+
+    K = segmented_images.shape[1]
+    if eval_fn == 'cond_entropy':
+        for i in range(K):
+            results = conditional_entropy_all(segmented_images[:, i], y)
+            avg_value = np.average(results)
+            max_value = np.max(results)
+            min_value = np.min(results)
+            avg_for_each_K.append(results)
+            if verbose:
+                print("average value of {} at K = {} is: avg: {}, max: {}, min: {}".format(eval_fn, n_clusters[i], avg_value, max_value, min_value))
+    elif eval_fn == 'f1':
+        for i in range(K):
+            results = f1_score_all(segmented_images[:, i], y)
+            avg_value = np.average(results)
+            max_value = np.max(results)
+            min_value = np.min(results)
+            avg_for_each_K.append(results)
+            if verbose:
+                print("average value of {} at K = {} is: avg: {}, max: {}, min: {}".format(eval_fn, n_clusters[i], avg_value, max_value, min_value))
+    else:
+        raise ValueError('invalid \'eval_fn\' value: expected: (f1 or cond_entropy)')
+
+    return avg_for_each_K
